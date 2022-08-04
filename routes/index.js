3294747dff0fd6d9e236a5a5eb8457d8f3893a5e
Diego Hernandez <diegofootball19@gmail.com>
2022-08-04 18:44:26 -0500

    added login and butyton

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const Story = require("../models/Story");
 // @desc Login/Landing page
 // @route GET/
-router.get("/", ensureGuest, (req, res) => {
+router.get("/", ensureGuest, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("errors/500");
+  }
   res.render("login", {
     layout: "login",
   });
